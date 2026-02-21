ffaf21c53e9cad9bb4f918e8ec94f0fbf0b8d8ca
paul-talma <109061871+paul-talma@users.noreply.github.com>
2026-02-21 10:19:27 -0800

    project 2 initial solution

--- projects/project2/src/gshare.cpp
+++ projects/project2/src/gshare.cpp
@@ -13,87 +13,136 @@
 // limitations under the License.
 //
 
-#include <iostream>
-#include <assert.h>
-#include <util.h>
-#include "types.h"
+#include "gshare.h"
 #include "core.h"
 #include "debug.h"
+#include "types.h"
+#include <algorithm>
+#include <assert.h>
+#include <cstdint>
+#include <iostream>
+#include <sys/types.h>
+#include <util.h>
 
 using namespace tinyrv;
 
 ////////////////////////////////////////////////////////////////////////////////
 
 GShare::GShare(uint32_t BTB_size, uint32_t BHR_size)
-  : BTB_(BTB_size, BTB_entry_t{false, 0x0, 0x0})
-  , PHT_((1 << BHR_size), 0x0)
-  , BHR_(0x0)
-  , BTB_shift_(log2ceil(BTB_size))
-  , BTB_mask_(BTB_size-1)
-  , BHR_mask_((1 << BHR_size)-1) {
-  //--
+    : BTB_(BTB_size, BTB_entry_t{false, 0x0, 0x0}), BHT_((1 << BHR_size), 0x0),
+      BHR_(0x0), BTB_shift_(log2ceil(BTB_size)), BTB_mask_(BTB_size - 1),
+      BHR_mask_((1 << BHR_size) - 1) {
+    //--
 }
 
 GShare::~GShare() {
-  //--
+    //--
 }
 
 uint32_t GShare::predict(uint32_t PC) {
-  uint32_t next_PC = PC + 4;
-  bool predict_taken = false;
+    uint32_t next_PC = PC + 4;
+    bool predict_taken = false;
 
-  // TODO:
+    // TODO:
+    // branch prediction
+    uint32_t bht_index = ((PC >> 2) ^ BHR_) & BHR_mask_;
+    predict_taken = (BHT_[bht_index] >= 2) ? 1 : 0;
 
-  DT(3, "*** GShare: predict PC=0x" << std::hex << PC << std::dec
-        << ", next_PC=0x" << std::hex << next_PC << std::dec
-        << ", predict_taken=" << predict_taken);
-  return next_PC;
+    // branch target
+    if (predict_taken) {
+        uint32_t btb_index = (PC >> 2) & BTB_mask_;
+        uint32_t btb_tag = (PC >> (BTB_shift_ + 2));
+        BTB_entry_t btb_row = BTB_[btb_index];
+        if (btb_row.valid && btb_row.tag == btb_tag) {
+            next_PC = btb_row.target;
+        }
+    }
+
+    DT(3, "*** GShare: predict PC=0x"
+              << std::hex << PC << std::dec << ", next_PC=0x" << std::hex
+              << next_PC << std::dec << ", predict_taken=" << predict_taken);
+    return next_PC;
 }
 
+/*
+ * PC is PC of current instruction
+ * next_PC is true (computed) next PC
+ * taken is prediction ground truth
+ */
 void GShare::update(uint32_t PC, uint32_t next_PC, bool taken) {
-  DT(3, "*** GShare: update PC=0x" << std::hex << PC << std::dec
-        << ", next_PC=0x" << std::hex << next_PC << std::dec
-        << ", taken=" << taken);
+    DT(3, "*** GShare: update PC=0x" << std::hex << PC << std::dec
+                                     << ", next_PC=0x" << std::hex << next_PC
+                                     << std::dec << ", taken=" << taken);
 
-  // TODO:
+    // TODO:
+    update_bht(PC, taken);
+    if (taken) {
+        update_btb(PC, next_PC);
+    }
+    update_bhr(taken);
+}
+
+void GShare::update_bht(uint32_t PC, bool taken) {
+    uint32_t bht_index =
+        ((PC >> 2) ^ BHR_) & BHR_mask_; // make sure bhr is current
+    if (taken) {
+        BHT_[bht_index] = inc_two_bit_counter(BHT_[bht_index]);
+    } else {
+        BHT_[bht_index] = dec_two_bit_counter(BHT_[bht_index]);
+    }
+}
+
+void GShare::update_btb(uint32_t PC, uint32_t next_PC) {
+    uint32_t btb_index = (PC >> 2) ^ BHR_;
+    BTB_entry_t row = BTB_[btb_index];
+    row.valid = true;
+    row.target = next_PC;
+    row.tag = (PC >> (BTB_shift_ + 2));
+}
+
+void GShare::update_bhr(bool taken) { BHR_ = BHR_ << 1 | taken; }
+
+uint32_t inc_two_bit_counter(uint32_t count) {
+    return count == 3 ? count : count++;
+}
+uint32_t dec_two_bit_counter(uint32_t count) {
+    return count == 0 ? count : count--;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 GSharePlus::GSharePlus(uint32_t BTB_size, uint32_t BHR_size) {
-  (void) BTB_size;
-  (void) BHR_size;
+    (void)BTB_size;
+    (void)BHR_size;
 }
 
 GSharePlus::~GSharePlus() {
-  //--
+    //--
 }
 
 uint32_t GSharePlus::predict(uint32_t PC) {
-  uint32_t next_PC = PC + 4;
-  bool predict_taken = false;
-  (void) PC;
-  (void) next_PC;
-  (void) predict_taken;
+    uint32_t next_PC = PC + 4;
+    bool predict_taken = false;
+    (void)PC;
+    (void)next_PC;
+    (void)predict_taken;
 
-  // TODO: extra credit component
+    // TODO: extra credit component
 
-  DT(3, "*** GShare+: predict PC=0x" << std::hex << PC << std::dec
-        << ", next_PC=0x" << std::hex << next_PC << std::dec
-        << ", predict_taken=" << predict_taken);
-  return next_PC;
+    DT(3, "*** GShare+: predict PC=0x"
+              << std::hex << PC << std::dec << ", next_PC=0x" << std::hex
+              << next_PC << std::dec << ", predict_taken=" << predict_taken);
+    return next_PC;
 }
 
 void GSharePlus::update(uint32_t PC, uint32_t next_PC, bool taken) {
-  (void) PC;
-  (void) next_PC;
-  (void) taken;
+    (void)PC;
+    (void)next_PC;
+    (void)taken;
 
-  DT(3, "*** GShare+: update PC=0x" << std::hex << PC << std::dec
-        << ", next_PC=0x" << std::hex << next_PC << std::dec
-        << ", taken=" << taken);
+    DT(3, "*** GShare+: update PC=0x" << std::hex << PC << std::dec
+                                      << ", next_PC=0x" << std::hex << next_PC
+                                      << std::dec << ", taken=" << taken);
 
-  // TODO: extra credit component
+    // TODO: extra credit component
 }
-
-
--- projects/project2/src/gshare.h
+++ projects/project2/src/gshare.h
@@ -16,47 +16,66 @@
 
 #pragma once
 
+#include <cstdint>
+#include <map>
+#include <sys/types.h>
 #include <vector>
 
 namespace tinyrv {
 
 class BranchPredictor {
-public:
-  virtual ~BranchPredictor() {}
+  public:
+    virtual ~BranchPredictor() {}
 
-  virtual uint32_t predict(uint32_t PC) {
-      return PC + 4;
-  };
+    virtual uint32_t predict(uint32_t PC) { return PC + 4; };
 
-  virtual void update(uint32_t PC, uint32_t next_PC, bool taken) {
-      (void) PC;
-      (void) next_PC;
-      (void) taken;
-  };
+    virtual void update(uint32_t PC, uint32_t next_PC, bool taken) {
+        (void)PC;
+        (void)next_PC;
+        (void)taken;
+    };
 };
 
 class GShare : public BranchPredictor {
-public:
-  GShare(uint32_t BTB_size, uint32_t BHR_size);
+  public:
+    GShare(uint32_t BTB_size, uint32_t BHR_size);
 
-  ~GShare() override;
+    ~GShare() override;
 
-  uint32_t predict(uint32_t PC) override;
-  void update(uint32_t PC, uint32_t next_PC, bool taken) override;
+    uint32_t predict(uint32_t PC) override;
+    void update(uint32_t PC, uint32_t next_PC, bool taken) override;
 
-  // TODO: Add your own methods here
+    // TODO: Add your own methods here
+    void update_bht(uint32_t PC, bool taken);
+    void update_btb(uint32_t PC, uint32_t next_PC);
+    void update_bhr(bool taken);
+    uint32_t inc_two_bit_counter(uint32_t count);
+    uint32_t dec_two_bit_counter(uint32_t count);
+
+  private:
+    struct BTB_entry_t {
+        bool valid;
+        uint32_t tag;
+        uint32_t target;
+    };
+    std::vector<BTB_entry_t> BTB_;
+    std::vector<uint32_t> BHT_;
+    uint32_t BHR_;
+    uint32_t BTB_shift_;
+    uint32_t BTB_mask_;
+    uint32_t BHR_mask_;
 };
 
 class GSharePlus : public BranchPredictor {
-public:
-  GSharePlus(uint32_t BTB_size, uint32_t BHR_size);
+  public:
+    GSharePlus(uint32_t BTB_size, uint32_t BHR_size);
 
-  ~GSharePlus() override;
+    ~GSharePlus() override;
 
-  uint32_t predict(uint32_t PC) override;
-  void update(uint32_t PC, uint32_t next_PC, bool taken) override;
+    uint32_t predict(uint32_t PC) override;
+    void update(uint32_t PC, uint32_t next_PC, bool taken) override;
 
-  // TODO: extra credit component
+    // TODO: extra credit component
 };
 
-}
+} // namespace tinyrv
